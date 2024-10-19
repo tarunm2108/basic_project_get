8dad5e8d9d4c605b40a8d97de7d048b6748aa5b8
Tarun <tarunm2108@gmail.com>
2024-10-19 14:17:06 +0530

    Utility code update.

--- lib/utils/utility.dart
+++ lib/utils/utility.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:basic_code_getx/utils/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:intl/intl.dart';
 
 class Utility {
 
@@ -44,5 +49,56 @@ class Utility {
    } else {
      return false;
    }
+ }
+
+
+ static String getTodayDate(String format) {
+   return DateFormat(format).format(DateTime.now());
+ }
+
+ static String formatDate(String date, String format, String expectFormat,
+     {bool? isUtc}) {
+   if (date.isEmpty || date.toLowerCase() == 'null') return 'N/A';
+   DateTime parse = DateFormat(format).parse(date, isUtc ?? false);
+   return DateFormat(expectFormat).format(parse.toLocal());
+ }
+
+ static DateTime stringToDateTime(String date, String format) {
+   DateTime parse = DateFormat(format).parse(date);
+   return parse;
+ }
+
+ static String timeFormat(int time) {
+   if (time >= 0 && time <= 9) {
+     return '0$time';
+   } else {
+     return '$time';
+   }
+ }
+
+ static bool getTimeDiff(String date) {
+   DateTime parse = DateFormat("yyyy-MM-dd hh:mm:ss").parse(date);
+   parse.add(const Duration(seconds: 15));
+   return DateTime.now().isBefore(parse);
+ }
+
+ static exitFromApp() {
+   try {
+     if (Platform.isAndroid) {
+       SystemNavigator.pop();
+     } else if (Platform.isIOS) {
+       exit(0);
+     }
+   } catch (e) {
+     Logger.instance.printError('____ $e');
+   }
+ }
+
+ static DateTime findFirstDateOfTheWeek(DateTime dateTime) {
+   return dateTime.subtract(Duration(days: dateTime.weekday - 1));
+ }
+
+ static DateTime findLastDateOfTheWeek(DateTime dateTime) {
+   return dateTime.add(Duration(days: DateTime.daysPerWeek - dateTime.weekday));
  }
 }
